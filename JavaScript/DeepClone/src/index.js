43ac6bf7b80f1ac99c2f33b86221c423569e4a55
Lee <326656669@qq.com>
2020-05-26 15:23:38 +0800

    feat : 优化深克隆代码

--- JavaScript/DeepClone/src/index.js
+++ JavaScript/DeepClone/src/index.js
@@ -1,42 +1,48 @@
-let cache = []
-
-function DeepClone (source) {
-  if (source instanceof Object) {
-    let cachedDist = findCache(source)
-    if (cachedDist) {
-      return cachedDist
-    } else {
-      let dist
-      if (source instanceof Array) {
-        dist = new Array()
-      } else if (source instanceof Function) {
-        dist = function () {
-          return source.call(this, ...arguments)
-        }
-      } else if (source instanceof RegExp) {
-        dist = new RegExp(source.source, source.flags)
-      } else if (source instanceof Date) {
-        dist = new Date(source)
+class DeepCloner {
+  constructor () {
+    this.cache = []
+  }
+  
+  clone (source) {
+    if (source instanceof Object) {
+      let cachedDist = this.findCache(source)
+      if (cachedDist) {
+        return cachedDist
       } else {
-        dist = new Object()
+        let dist
+        if (source instanceof Array) {
+          dist = new Array()
+        } else if (source instanceof Function) {
+          dist = function () {
+            return source.call(this, ...arguments)
+          }
+        } else if (source instanceof RegExp) {
+          dist = new RegExp(source.source, source.flags)
+        } else if (source instanceof Date) {
+          dist = new Date(source)
+        } else {
+          dist = new Object()
+        }
+        this.cache.push([source, dist])
+        for (let key in source) {
+          if (source.hasOwnProperty(key)) {
+            dist[key] = this.clone(source[key])
+          }
+        }
+        return dist
       }
-      cache.push([source, dist])
-      for (let key in source) {
-        dist[key] = DeepClone(source[key])
-      }
-      return dist
     }
+    return source
   }
-  return source
+  
+  findCache (source) {
+    for (let i = 0; i < this.cache.length; i++) {
+      if (this.cache[i][0] === source) {
+        return this.cache[i][1]
+      }
+    }
+    return undefined
+  }
 }
 
-function findCache (source) {
-  for (let i = 0; i < cache.length; i++) {
-    if (cache[i][0] === source) {
-      return cache[i][1]
-    }
-  }
-  return undefined
-}
-
-module.exports = DeepClone
+module.exports = DeepCloner
--- JavaScript/DeepClone/test/index.js
+++ JavaScript/DeepClone/test/index.js
@@ -4,53 +4,53 @@ const sinonChai = require('sinon-chai')
 chai.use(sinonChai)
 
 const assert = chai.assert
-const DeepClone = require('../src/index')
+const DeepCloner = require('../src/index')
 
-describe('DeepClone', () => {
-  it('是一个函数', () => {
-    assert.isFunction(DeepClone)
+describe('', () => {
+  it('是一个类', () => {
+    assert.isFunction(DeepCloner)
   })
   
   it('能够复制基本类型 number', () => {
     const n = 123
-    const n2 = DeepClone(n)
+    const n2 = new DeepCloner().clone(n)
     assert(n === n2)
   })
   
   it('能够复制基本类型 string', () => {
     const s = '123'
-    const s2 = DeepClone(s)
+    const s2 = new DeepCloner().clone(s)
     assert(s === s2)
   })
   
   it('能够复制基本类型 boolean', () => {
     const b = true
-    const b2 = DeepClone(b)
+    const b2 = new DeepCloner().clone(b)
     assert(b === b2)
   })
   
   it('能够复制基本类型 undefined', () => {
     const u = undefined
-    const u2 = DeepClone(u)
+    const u2 = new DeepCloner().clone(u)
     assert(u === u2)
   })
   
   it('能够复制基本类型 null', () => {
     const empty = null
-    const empty2 = DeepClone(empty)
+    const empty2 = new DeepCloner().clone(empty)
     assert(empty === empty2)
   })
   
   it('能够复制基本类型 symbol', () => {
     const sym = Symbol()
-    const sym2 = DeepClone(sym)
+    const sym2 = new DeepCloner().clone(sym)
     assert(sym === sym2)
   })
   
   describe('对象', () => {
     it('能够复制普通对象', () => {
       const a = {name: 'lzy', child: {name: 'junior lzy'}}
-      const a2 = DeepClone(a)
+      const a2 = new DeepCloner().clone(a)
       assert(a !== a2)
       assert(a.name === a2.name)
       assert(a.child !== a2.child)
@@ -59,7 +59,7 @@ describe('DeepClone', () => {
     
     it('能够复制数组对象', () => {
       const a = [[11, 12], [21, 22], [31, 32]]
-      const a2 = DeepClone(a)
+      const a2 = new DeepCloner().clone(a)
       assert(a !== a2)
       assert(a[0] !== a2[0])
       assert(a[1] !== a2[1])
@@ -70,7 +70,7 @@ describe('DeepClone', () => {
     it('能够复制函数对象', () => {
       const a = function (x, y) {return x + y}
       a.xxx = {yyy: {zzz: 1}}
-      const a2 = DeepClone(a)
+      const a2 = new DeepCloner().clone(a)
       assert(a !== a2)
       assert(a.xxx.yyy.zzz === a2.xxx.yyy.zzz)
       assert(a.xxx.yyy !== a2.xxx.yyy)
@@ -82,7 +82,7 @@ describe('DeepClone', () => {
       // const a = {name:'lzy' , self:a} // a.self === undefined 左查询 右查询 赋值后操作 等号右边先操作
       const a = {name: 'lzy'}
       a.self = a
-      const a2 = DeepClone(a)
+      const a2 = new DeepCloner().clone(a)
       assert(a !== a2)
       assert(a.name === a2.name)
       assert(a.self !== a2.self)
@@ -97,7 +97,7 @@ describe('DeepClone', () => {
         }
         b = b.child
       }
-      const a2 = DeepClone(a)
+      const a2 = new DeepCloner().clone(a)
       assert(a !== a2)
       assert(a.child !== a2.child)
     })
@@ -106,7 +106,7 @@ describe('DeepClone', () => {
       //   const a = /hi\d+/gi;
       const a = new RegExp('hi\\d+', 'gi')
       a.xxx = {yyy: {zzz: 1}}
-      const a2 = DeepClone(a)
+      const a2 = new DeepCloner().clone(a)
       assert(a.source === a2.source)
       assert(a.flags === a2.flags)
       assert(a !== a2)
@@ -119,12 +119,82 @@ describe('DeepClone', () => {
       //   const a = /hi\d+/gi;
       const a = new Date()
       a.xxx = {yyy: {zzz: 1}}
-      const a2 = DeepClone(a)
+      const a2 = new DeepCloner().clone(a)
       assert(a !== a2)
       assert(a.getTime() === a2.getTime())
       assert(a.xxx.yyy.zzz === a2.xxx.yyy.zzz)
       assert(a.xxx.yyy !== a2.xxx.yyy)
       assert(a.xxx !== a2.xxx)
+    })
+    
+    it('自动跳过原型属性', () => {
+      const a = Object.create({yyy: {zzz: 1}})
+      const a2 = new DeepCloner().clone(a)
+      assert(a !== a2)
+      assert.isFalse('name' in a2)
+    })
+    
+    it('很复杂的对象', () => {
+      const a = {
+        n: NaN,
+        n2: Infinity,
+        s: '',
+        bool: false,
+        null: null,
+        u: undefined,
+        sym: Symbol(),
+        o: {
+          n: NaN,
+          n2: Infinity,
+          s: '',
+          bool: false,
+          null: null,
+          u: undefined,
+          sym: Symbol(),
+        },
+        array: [
+          {
+            n: NaN,
+            n2: Infinity,
+            s: '',
+            bool: false,
+            null: null,
+            u: undefined,
+            sym: Symbol(),
+          },
+        ],
+        fn: function () {
+          return 'fn'
+        },
+        data: new Date(),
+        reg: /test/gi
+      }
+      const a2 = new DeepCloner().clone(a)
+      assert(a !== a2)
+      assert.isNaN(a2.n)
+      assert(a.n2 === a2.n2)
+      assert(a.s === a2.s)
+      assert(a.bool === a2.bool)
+      assert(a.null === a2.null)
+      assert(a.u === a2.u)
+      assert(a.sym === a2.sym)
+      assert(a.o !== a2.o)
+      assert.isNaN(a2.o.n)
+      assert(a.o.n2 === a2.o.n2)
+      assert(a.o.s === a2.o.s)
+      assert(a.o.bool === a2.o.bool)
+      assert(a.o.null === a2.o.null)
+      assert(a.o.u === a2.o.u)
+      assert(a.o.sym === a2.o.sym)
+      assert(a.array !== a2.array)
+      assert(a.array[0] !== a2.array[0])
+      assert.isNaN(a2.array[0].n)
+      assert(a.array[0].n2 === a2.array[0].n2)
+      assert(a.array[0].s === a2.array[0].s)
+      assert(a.array[0].bool === a2.array[0].bool)
+      assert(a.array[0].null === a2.array[0].null)
+      assert(a.array[0].u === a2.array[0].u)
+      assert(a.array[0].sym === a2.array[0].sym)
     })
   })
 })
