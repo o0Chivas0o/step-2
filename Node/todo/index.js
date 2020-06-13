e03a6e550340f3cd3f85da76a20c3a6cba06e35a
Lee <326656669@qq.com>
2020-06-13 17:29:13 +0800

    feat : 添加自定义指令

--- Node/todo/index.js
+++ Node/todo/index.js
@@ -1,1 +1,21 @@
-console.log('hi,i am node.js')
+const {program} = require('commander')
+
+program
+.option('-x,--xxx', 'what the x')
+program
+.command('add')
+.description('add a task')
+.action((...args) => {
+  const words = args.pop().join(' ')
+  console.log(words)
+})
+program
+.command('clear')
+.description('clear all task')
+.action((...args) => {
+  const words = args.pop().join(' ')
+  console.log(words)
+})
+
+program.parse(process.argv)
+
