776a31c2b72c9317b0e5997bbc0096982ee53614
Lee <326656669@qq.com>
2020-06-17 20:58:17 +0800

    feat : 优化add

--- Node/todo/index.js
+++ Node/todo/index.js
@@ -1,34 +1,15 @@
 const homedir = require('os').homedir()
 const home = process.env.HOME || homedir
 const p = require('path')
-const fs = require('fs')
 const dbPath = p.join(home, '.todo')
+const fs = require('fs')
+const db = require('./db.js')
 
-module.exports.add = (title) => {
+module.exports.add = async (title) => {
   // 读取之前的任务
-  fs.readFile(dbPath, {flag: 'a+'}, (error, data) => {
-    if (error) {console.log(error)} else {
-      let list
-      try {
-        list = JSON.parse(data.toString())
-        console.log(list)
-      }
-      catch (error2) {
-        list = []
-      }
-      console.log(list)
-      const task = {
-        title,
-        done: false
-      }
-      list.push(task)
-      console.log(list)
-      const string = JSON.stringify(list)
-      fs.writeFile(dbPath, string + '\n', (error3) => {
-        if (error3) {console.log(error3)}
-      })
-    }
-  })
+  const list = await db.read(dbPath)
   // 往里面添加一个 title 任务
+  list.push({title, done: false})
   // 存储任务到文件
+  await db.write(list)
 }
