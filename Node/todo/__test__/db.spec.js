22589ce2a2f5cde776bec10b34f48a8eac64e416
Lee <326656669@qq.com>
2020-07-13 14:01:42 +0800

    feat : 完成 write 测试

--- Node/todo/__test__/db.spec.js
+++ Node/todo/__test__/db.spec.js
@@ -5,11 +5,18 @@ jest.mock('fs')
 describe('db', () => {
   it('can read', async () => {
     const data = {title: 'hi', done: true}
-    fs.setMock('/xxx', null, JSON.stringify(data))
+    fs.setReadFileMocks('/xxx', null, JSON.stringify(data))
     const list = await db.read('/xxx')
     expect(list).toStrictEqual(data)
   })
-  it('can write', () => {
-    expect(db.write instanceof Function).toBe(true)
+  it('can write', async () => {
+    let fakeFile = ''
+    fs.setWriteFileMocks('/yyy', (path, data, callback) => {
+      fakeFile = data
+      callback(null)
+    })
+    const list = [{title: 'xxx', done: true}, {title: 'yyy', done: true}]
+    await db.write(list, '/yyy')
+    expect(fakeFile).toBe(JSON.stringify(list) + '\n')
   })
 })
