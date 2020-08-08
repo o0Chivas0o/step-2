7d5d98338333dd402047c890f2632f94487f3b13
Lee <326656669@qq.com>
2020-08-08 21:50:26 +0800

    feat : 第一版 和 5个测试用例

---
+++ JavaScript/Promise/src/promise.ts
@@ -0,0 +1,27 @@
+class Promise2 {
+  succeed = null
+  fail = null
+
+  resolve() {
+    setTimeout(() => {
+      this.succeed()
+    }, 0)
+  }
+  reject() {
+    setTimeout(() => {
+      this.fail()
+    }, 0)
+  }
+  constructor(fn) {
+    if (typeof fn !== 'function') {
+      throw new Error("我只接受函数")
+    }
+    fn(this.resolve.bind(this), this.reject.bind(this))
+  }
+  then(succeed, fail) {
+    this.succeed = succeed
+    this.fail = fail
+  }
+}
+
+export default Promise2
--- JavaScript/Promise/test/index.ts
+++ JavaScript/Promise/test/index.ts
@@ -1,10 +1,59 @@
 import * as chai from 'chai'
+import Promise from '../src/promise'
 
 const assert = chai.assert
 
-describe("Chai 的使用", () => {
-	it("测试可以相等", () => {
-		assert(1 === 1)
-	})
+describe("Promise", () => {
+  it("是一个类", () => {
+    assert.isFunction(Promise)
+    assert.isObject(Promise.prototype)
+  })
+  it("new Promise 如果接受不是一个函数就报错", () => {
+    assert.throw(() => {
+      //@ts-ignore
+      new Promise()
+    })
+    assert.throw(() => {
+      //@ts-ignore
+      new Promise(1)
+    })
+  })
+  it("new Promise(fn) 会生成一个对象,对象有 then 方法", () => {
+    const promise = new Promise(() => {})
+    assert.isFunction(promise.then)
+  })
+  it("new Promise(fn) 中的 fn 立即执行", () => {
+    let called: boolean = false
+    const promise = new Promise(() => {
+      called = true
+    })
+    // @ts-ignore
+    assert(called === true)
+  })
+  it("new Promise(fn) 中的 fn 执行的时候接受 resolve 和 reject 两个函数", () => {
+    let called: boolean = false
+    const promise = new Promise((resolve, reject) => {
+      called = true
+      assert.isFunction(resolve)
+      assert.isFunction(reject)
+    })
+    // @ts-ignore
+    assert(called === true)
+  })
+  it("promise.then(success) 中的 success 会在 resolve 被调用的时候执行", (done) => {
+    let called = false
+    const promise = new Promise((resolve, reject) => {
+      assert(called === false)
+      resolve()
+      setTimeout(() => {
+        assert(called === true)
+        done()
+      })
+    })
+    // @ts-ignore
+    promise.then(() => {
+      called = true
+    })
+  })
 })
 
