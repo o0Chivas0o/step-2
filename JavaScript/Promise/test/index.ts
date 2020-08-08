0f3e66677f26a4e676e897f4ed5c9b5ea15807ef
Lee <326656669@qq.com>
2020-08-08 22:12:34 +0800

    feat : 添加 sinon 作为函数测试

--- JavaScript/Promise/test/index.ts
+++ JavaScript/Promise/test/index.ts
@@ -1,6 +1,9 @@
 import * as chai from 'chai'
+import * as sinon from 'sinon'
+import * as sinonChai from 'sinon-chai';
 import Promise from '../src/promise'
 
+chai.use(sinonChai)
 const assert = chai.assert
 
 describe("Promise", () => {
@@ -23,37 +26,30 @@ describe("Promise", () => {
     assert.isFunction(promise.then)
   })
   it("new Promise(fn) 中的 fn 立即执行", () => {
-    let called: boolean = false
-    const promise = new Promise(() => {
-      called = true
-    })
+    const fn = sinon.fake()
+    const promise = new Promise(fn)
     // @ts-ignore
-    assert(called === true)
+    assert(fn.called)
   })
-  it("new Promise(fn) 中的 fn 执行的时候接受 resolve 和 reject 两个函数", () => {
-    let called: boolean = false
+  it("new Promise(fn) 中的 fn 执行的时候接受 resolve 和 reject 两个函数", (done) => {
     const promise = new Promise((resolve, reject) => {
-      called = true
       assert.isFunction(resolve)
       assert.isFunction(reject)
+      done()
     })
-    // @ts-ignore
-    assert(called === true)
   })
   it("promise.then(success) 中的 success 会在 resolve 被调用的时候执行", (done) => {
-    let called = false
+    const success = sinon.fake()
     const promise = new Promise((resolve, reject) => {
-      assert(called === false)
+      assert.isFalse(success.called)
       resolve()
       setTimeout(() => {
-        assert(called === true)
+        assert.isTrue(success.called)
         done()
       })
     })
     // @ts-ignore
-    promise.then(() => {
-      called = true
-    })
+    promise.then(success)
   })
 })
 
