04c488da3732bdc6fd58347deaac0f95b3e40c4e
Lee <326656669@qq.com>
2020-11-09 17:43:06 +0800

    feat(class): class inherit && combination

--- JavaScript/Class/src/index.ts
+++ JavaScript/Class/src/index.ts
@@ -1,6 +1,31 @@
-class Person {
-  sayHi(): void {}
-  constructor(public name: string, public age: number) {}
+// Class(类)
+// 1.声明对象的自身属性(非函数)
+// 2.声明对象的共有属性
+// 3.声明对象的自身函数
+// 4.声明类的自身属性(可以是函数)
+
+// 继承
+//
+//
+//
+
+class EventEmitter {
+  constructor() {}
+  cache: Array<string> = []
+  on(): void {}
+  off(): void {}
+  emit(): void {}
 }
-let person1 = new Person('xxx',21)
-let person2 = new Person('xxy',22)
+
+class Person extends EventEmitter {
+  constructor(public name: string, public age: number) {super()}
+  sayHi(): void {}
+}
+
+class Newspaper extends EventEmitter {
+  constructor() {super();}
+  // 多态 子类 on 有自身的状态
+  on() {
+    console.log('xxx')
+  }
+}
