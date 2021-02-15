0d77c22280abbb9db8ac8b5143ed3c454c5de461
Lee <326656669@qq.com>
2021-02-15 17:04:47 +0800

    refactor: change code style

--- webpack/webpack-demo2/dist.js
+++ webpack/webpack-demo2/dist.js
@@ -1,69 +1,73 @@
-var depRelation = [{
-  key: "index.js",
-  deps: ["a.js", "b.js"],
-  code: function (require, module, exports) {
-    "use strict";
-
-    var _a = _interopRequireDefault(require("./a.js"));
-
-    var _b = _interopRequireDefault(require("./b.js"));
-
-    function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-    console.log(_a["default"].getB());
-    console.log(_b["default"].getA());
-  }
-}, {
-  key: "a.js",
-  deps: ["b.js"],
-  code: function (require, module, exports) {
-    "use strict";
-
-    Object.defineProperty(exports, "__esModule", {
-      value: true
-    });
-    exports["default"] = void 0;
-
-    var _b = _interopRequireDefault(require("./b.js"));
-
-    function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-    var a = {
-      value: 'a',
-      getB: function getB() {
-        return _b["default"].value + ' from a.js';
+var depRelation = [
+  {
+    key: 'index.js',
+    deps: ['a.js', 'b.js'],
+    code: function (require, module, exports) {
+      'use strict'
+      
+      var _a = _interopRequireDefault(require('./a.js'))
+      
+      var _b = _interopRequireDefault(require('./b.js'))
+      
+      function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj } }
+      
+      console.log(_a['default'].getB())
+      console.log(_b['default'].getA())
+    }
+  },
+  {
+    key: 'a.js',
+    deps: ['b.js'],
+    code: function (require, module, exports) {
+      'use strict'
+      
+      Object.defineProperty(exports, '__esModule', {
+        value: true
+      })
+      exports['default'] = void 0
+      
+      var _b = _interopRequireDefault(require('./b.js'))
+      
+      function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj } }
+      
+      var a = {
+        value: 'a',
+        getB: function getB() {
+          return _b['default'].value + ' from a.js'
+        }
       }
-    };
-    var _default = a;
-    exports["default"] = _default;
-  }
-}, {
-  key: "b.js",
-  deps: ["a.js"],
-  code: function (require, module, exports) {
-    "use strict";
-
-    Object.defineProperty(exports, "__esModule", {
-      value: true
-    });
-    exports["default"] = void 0;
-
-    var _a = _interopRequireDefault(require("./a.js"));
-
-    function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-    var b = {
-      value: 'b',
-      getA: function getA() {
-        return _a["default"].value + ' from b.js';
+      var _default = a
+      exports['default'] = _default
+    }
+  },
+  {
+    key: 'b.js',
+    deps: ['a.js'],
+    code: function (require, module, exports) {
+      'use strict'
+      
+      Object.defineProperty(exports, '__esModule', {
+        value: true
+      })
+      exports['default'] = void 0
+      
+      var _a = _interopRequireDefault(require('./a.js'))
+      
+      function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj } }
+      
+      var b = {
+        value: 'b',
+        getA: function getA() {
+          return _a['default'].value + ' from b.js'
+        }
       }
-    };
-    var _default = b;
-    exports["default"] = _default;
-  }
-}]
+      var _default = b
+      exports['default'] = _default
+    }
+  }]
 var modules = {}
 execute(depRelation[0].key)
+
 function execute(key) {
   // 如果已经 require 过，就直接返回上次的结果
   if (modules[key]) { return modules[key] }
